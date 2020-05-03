0640f2114824814c802eb0706f1c24b8d974c309
adhi-27 <adhityasuresh59@gmail.com>
2020-05-03 20:56:39 +0530

    Todo manager desing with HTML CSS

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,4 +1,4 @@
-class Todo < ActiveRecord::Base
+class Todo < ApplicationRecord
   def to_pleasant_string
     is_completed = completed ? "[X]" : "[]"
     "#{id}.  #{due_date.to_s(:short)}  #{todo_text}  #{is_completed}"
