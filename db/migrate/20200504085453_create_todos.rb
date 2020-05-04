a8ac510d117ee7ada8e80265003c9969745a7c79
adhi-27 <adhityasuresh59@gmail.com>
2020-05-04 14:26:56 +0530

    Updated database migrations

--- db/migrate/20200504085453_create_todos.rb
+++ db/migrate/20200504085453_create_todos.rb
@@ -1,10 +1,9 @@
 class CreateTodos < ActiveRecord::Migration[6.0]
   def change
     create_table :todos do |t|
-      t.string :todo_text
+      t.text :todo_text
       t.date :due_date
       t.boolean :completed
-      t.timestamps
     end
   end
 end
