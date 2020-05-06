a197d836fd6649f24ca60909a766f39f0eca8f32
adhi-27 <adhityasuresh59@gmail.com>
2020-05-06 23:20:54 +0530

    Ensure no empty name email or password in users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,12 @@ class UsersController < ApplicationController
     lname = params[:lname]
     mail = params[:email]
     pass = params[:password]
-    new_user = User.create!(fname: fname, lname: lname, email: mail, password: pass)
-    redirect_to "/"
+    new_user = User.new(fname: fname, lname: lname, email: mail, password: pass)
+    if !new_user.save
+      flash[:error] = new_user.errors.full_messages.join(", ")
+      redirect_to new_user_path
+    else
+      redirect_to "/"
+    end
   end
 end
